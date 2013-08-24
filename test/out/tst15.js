6b1172821954b6dd6e211b9499b583b1ccd5bd36
Max Schaefer <schaefer@ntu.edu.sg>
2013-08-24 13:07:03 +0800

    Better dependency tracking when unfolding ASG into AST.

--- test/out/tst15.js
+++ test/out/tst15.js
@@ -1,15 +1,15 @@
 var Deferred;
 (function() {
 	var function_63_1, function_142_1;
+	var function_63 = function(x1) {
+		function_63_1 = x1;
+		return obj_128;
+	};
 	var obj_128 = {
 		resolve: function(x1) {
 			function_142_1 = x1;
 		},
 		done: function_63
-	};
-	var function_63 = function(x1) {
-		function_63_1 = x1;
-		return obj_128;
 	};
 	Deferred = function() {
 		return obj_128;
