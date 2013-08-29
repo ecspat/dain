bcaf94adcc32fb5a24c601fd26f2410bc1dc45ea
Max Schaefer <schaefer@ntu.edu.sg>
2013-08-29 18:42:32 +0800

    Added --no-cb flag.

--- build-model.js
+++ build-model.js
@@ -17,16 +17,31 @@
 var ModelBuilder = require('./lib/build-model/ModelBuilder'),
     fs = require('fs');
     
+function pushAll(a, b) {
+	for(var i=0,n=b.length;i<n;++i) {
+		a[a.length] = b[i];
+	}
+}
+
+var no_cb = false, i = 2, n;
+
+if(process.argv[i] === '--no-cb') {
+	no_cb = true;
+	++i;
+}
+    
 var events = {
 	prop_writes: [],
 	returns: [],
 	callbacks: []
 };
 
-for(var i=2,n=process.argv.length;i<n;++i) {
+for(n=process.argv.length;i<n;++i) {
 	var new_events = JSON.parse(fs.readFileSync(process.argv[i], 'utf-8'));
-	['prop_writes', 'returns', 'callbacks'].forEach(function(p) {
-		Array.prototype.push.apply(events[p], new_events[p]);
-	});
+	pushAll(events.prop_writes, new_events.prop_writes);
+	pushAll(events.returns, new_events.returns);
+	if(!no_cb) {
+		pushAll(events.callbacks, new_events.callbacks);
+	}
 }
 console.log(ModelBuilder.buildModel(events));
