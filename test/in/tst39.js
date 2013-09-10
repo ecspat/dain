e0f904384cd30ab70d6794f592526b645d2fc2dd
Max Schaefer <schaefer@ntu.edu.sg>
2013-09-10 17:50:06 +0800

    Slightly improved ASG generation for prototype property of functions: omit any empty object literals.

---
+++ test/in/tst39.js
@@ -0,0 +1,4 @@
+function f() {}
+f.prototype = {
+	foo: 'bar'
+};
---
+++ test/out/tst39.js
@@ -0,0 +1,8 @@
+var f;
+(function () {
+    var function_0 = function function_0() {
+    };
+    function_0.prototype = { foo: String(Math.random()) };
+    f = function_0;
+}());
+/*{"0":[0]}*/
